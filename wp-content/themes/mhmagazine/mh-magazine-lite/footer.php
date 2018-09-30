d9aaea2e4857484ea8b69491d3720c7137eb33f8
Sinh <sinh.buidinh@gmail.com>
2018-09-30 17:37:35 +0700

    add footer info

--- wp-content/themes/mhmagazine/mh-magazine-lite/footer.php
+++ wp-content/themes/mhmagazine/mh-magazine-lite/footer.php
@@ -1,5 +1,40 @@
 <?php mh_before_footer(); ?>
 <?php mh_magazine_lite_footer_widgets(); ?>
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<div class="wrap">
+		<aside class="widget-area" role="complementary" aria-label="Footer">
+	            <div class="widget-column footer-widget-1">
+	                <section id="text-8" class="widget widget_text">
+	                    <h2 class="widget-title">Liên hệ:</h2>
+	                    <div class="textwidget">
+	                        <p>
+	                            <strong>Địa chỉ:</strong>
+	                            <br> Thầy Loan, cổng chùa Cầu, Xã Mỹ Thái
+	                            <br> Huyện Lạng Giang, Tỉnh Bắc Giang
+	                        </p>
+	                    </div>
+	                </section>
+	            </div>
+	            <div class="widget-column footer-widget-2">
+	                <section id="text-9" class="widget widget_text">
+	                    <div class="textwidget">
+	                        <p>
+	                            <strong>Giờ làm việc:</strong>
+	                            <br> <b>T2—T6:</b> 9:00–17:00
+	                            <br> <b>T7 &amp; CN:</b> 11:00–15:00
+	                        </p>
+	                        <p>
+	                            <strong>Nguyễn Quốc Loan</strong>
+	                            <br>
+	                            <strong>SĐT:<strong> 039.290.8306
+	                        	</strong></strong>
+	                    	</p>
+	                	</div>
+	            	</section>
+	        	</div>
+	    </aside>
+	</div>
+</footer>
 <div class="mh-copyright-wrap">
 	<div class="mh-container mh-container-inner mh-clearfix">
 		<p class="mh-copyright"><?php printf(esc_html__('Copyright &copy; %1$s | Make by %2$s', 'mh-magazine-lite'), date("Y"), 'SINHBD'); ?></p>
